820528d57ea80bfd79669aa30820338516e1a47c
AlbertoCanezin <alberto.canezin@ncpc.com.br>
2022-07-07 09:44:56 -0300

    Programa console para calculo de contratos

---
+++ Entities/Department.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ProgramConsole.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department() {}
+
+        public Department(string name) 
+        {
+            Name = name;
+        }
+    }
+}
